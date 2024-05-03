1165100903d17b22aa13f0eb443b2b2da06e9573
srinija684 <75112894+srinijag684@users.noreply.github.com>
2024-05-03 13:08:19 +0800

    Keycloak auth

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}> Welcome To LetMeQ </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CBC3E3',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect, useRef } from "react";
+import Keycloak from "keycloak-js";
+
+const client = new Keycloak({});
+
